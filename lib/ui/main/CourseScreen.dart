1cecfc1902936fadfceda28bbcc5d7afd9514772
Tomáš Sobota <67016313+sobotat@users.noreply.github.com>
2023-08-06 13:25:28 +0200

    Updated CourseScreen.dart AppBar
    Took 39 minutes

--- lib/ui/main/CourseScreen.dart
+++ lib/ui/main/CourseScreen.dart
@@ -4,6 +4,10 @@ import 'package:oes/config/AppTheme.dart';
 import 'package:oes/src/AppSecurity.dart';
 import 'package:oes/src/Objects/Course.dart';
 import 'package:oes/src/RestApi/CourseGateway.dart';
+import 'package:oes/ui/assets/buttons/Sign-OutButton.dart';
+import 'package:oes/ui/assets/buttons/ThemeModeButton.dart';
+import 'package:oes/ui/assets/buttons/UserInfoButton.dart';
+import 'package:oes/ui/assets/dialogs/SmallMenu.dart';
 
 class CourseScreen extends StatefulWidget {
   const CourseScreen({
@@ -53,8 +57,38 @@ class _CourseScreenState extends State<CourseScreen> {
 
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    var overflow = 950;
+
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.secondary,
+        actions: [
+          Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 5,
+              vertical: 0,
+            ),
+            child: width > overflow ? const Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      UserInfoButton(
+                        width: 150,
+                      ),
+                      SignOutButton(),
+                      ThemeModeButton(),
+                    ],
+                  ),
+                ),
+              ],
+            ) : const SmallMenu(),
+          ),
+        ],
+      ),
       body: !isInit ? Center(
         child: CircularProgressIndicator(
           color: Theme.of(context).extension<AppCustomColors>()!.accent,
