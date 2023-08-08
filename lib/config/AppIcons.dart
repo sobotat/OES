e8c036fc212f81046774c6bc1a645f1deb9cbcfc
Tomáš Sobota <67016313+sobotat@users.noreply.github.com>
2023-08-08 17:58:14 +0200

    Modified Icons
    Took 2 hours 20 minutes

--- lib/config/AppIcons.dart
+++ lib/config/AppIcons.dart
@@ -11,4 +11,9 @@ class AppIcons {
   static const IconData icon_teapot = IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData icon_sign_out = IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData icon_profile = IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData icon_apple = IconData(0xe805, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData icon_ios = IconData(0xe806, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData icon_web = IconData(0xe807, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData icon_windows = IconData(0xe808, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData icon_android = IconData(0xe809, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 }
