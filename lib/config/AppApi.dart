382afa6c6a9ebe1d97fbb413af9adf067944ecd0
Tomáš Sobota <67016313+sobotat@users.noreply.github.com>
2023-09-19 20:01:15 +0200

    Added mainServerUrl and apiServerUrl
    Took 4 minutes

--- lib/config/AppApi.dart
+++ lib/config/AppApi.dart
@@ -7,8 +7,10 @@ class AppApi {
 
   bool useMuckApi = true;
 
+  String mainServerUrl = 'http://oes-main.sobotovi.net:8002';
+  String apiServerUrl = 'http://oes-api.sobotovi.net:8001';
+
   Future<void> init() async {
-    if (useMuckApi) return;
     useMuckApi = (await LocalStorage.instance.get('useMuck') ?? 'false') as bool;
   }
 }
