48694b2fcea44379081fe41fe7faa12fbd913552
Tomáš Sobota <67016313+sobotat@users.noreply.github.com>
2023-09-25 11:36:08 +0200

    Implemented basic test user login Api
    Took 22 seconds

--- lib/src/restApi/api/ApiUserGateway.dart
+++ lib/src/restApi/api/ApiUserGateway.dart
@@ -8,48 +8,64 @@ import 'package:oes/src/restApi/UserGateway.dart';
 import 'package:oes/src/restApi/api/http/HttpRequest.dart';
 import 'package:oes/src/restApi/api/http/HttpRequestOptions.dart';
 import 'package:oes/src/restApi/api/http/RequestResult.dart';
+import 'package:oes/src/services/LocalStorage.dart';
 
 class ApiUserGateway implements UserGateway {
 
-  String basePath = '${AppApi.instance.apiServerUrl}/api/course';
+  String basePath = '${AppApi.instance.apiServerUrl}/api/user';
 
   @override
   Future<SignedUser?> loginWithToken(String token) async {
-    RequestResult result = await HttpRequest.instance.get('$basePath', options: HttpRequestOptions(
-      headers: {
-        'Authorization': 'eyJVc2VySWQiOjEsIlVzZXJSb2xlIjoidGVhY2hlciIsIkNyZWF0aW9uIjoiMjAyMy0wOS0yM1QyMjo1Nzo0OS41OTM0NTg2KzAyOjAwIiwiRXhwaXJhdGlvbiI6IjIwMjMtMDktMjNUMjI6NTg6MzQuNTkzNDY0NCswMjowMCJ9|2360329f-4078-4107-8788-49ee8db003fc'
+    RequestResult result = await HttpRequest.instance.get(basePath,
+      queryParameters: {
+        'token': token,
       }
-    ));
+    );
 
     if (result.statusCode != 200 || result.data is! Map<String, dynamic>) {
       debugPrint('Api Error: [User-loginWithToken] ${result.statusCode} -> ${result.message}');
       return null;
     }
 
-    return SignedUser.fromJson(result.data);
+    try {
+      SignedUser user = SignedUser.fromJson(result.data);
+      await LocalStorage.instance.set('token', user.token);
+
+      return user;
+    } on Exception {
+      return null;
+    }
   }
 
   @override
   Future<SignedUser?> loginWithUsernameAndPassword(String username, String password, bool rememberMe, Device device) async {
-    RequestResult result = await HttpRequest.instance.get('$basePath/', queryParameters: {
-      'username': username,
-      'password': password,
-      'rememberMe': rememberMe,
-      'device': device.toMap()
-    },);
+    RequestResult result = await HttpRequest.instance.get(basePath,
+      queryParameters: {
+        'username': username,
+        'password': password,
+        'rememberMe': rememberMe,
+        'device': device.toMap()
+      },
+    );
 
     if (result.statusCode != 200 || result.data is! Map<String, dynamic>) {
       debugPrint('Api Error: [User-loginWithToken] ${result.statusCode} -> ${result.message}');
       return null;
     }
 
-    return SignedUser.fromJson(result.data);
+    try {
+      SignedUser user = SignedUser.fromJson(result.data);
+      await LocalStorage.instance.set('token', user.token);
+
+      return user;
+    } on Exception {
+      return null;
+    }
   }
 
   @override
-  Future<void> logout() {
-    // TODO: implement logout
-    throw UnimplementedError();
+  Future<void> logout() async {
+    LocalStorage.instance.remove('token');
   }
 
   @override
