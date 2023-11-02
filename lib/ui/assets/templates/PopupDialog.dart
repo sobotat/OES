df910eb48c22f0c609cafce840a4b7148529c39c
Tomáš Sobota <67016313+sobotat@users.noreply.github.com>
2023-11-02 14:53:17 +0100

    Added PagedData.dart
    Took 6 seconds

--- lib/ui/assets/templates/PopupDialog.dart
+++ lib/ui/assets/templates/PopupDialog.dart
@@ -3,17 +3,19 @@ import 'package:flutter/material.dart';
 class PopupDialog extends Dialog {
   const PopupDialog({
     this.alignment = Alignment.center,
+    this.padding = const EdgeInsets.all(10),
     required this.child,
     super.key
   });
 
   final Widget child;
   final Alignment alignment;
+  final EdgeInsets padding;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(10),
+      padding: padding,
       child: Align(
         alignment: alignment,
         child: child
