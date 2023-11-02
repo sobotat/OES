dc13bce5e133cfa557fc1e2c3afcc47dabff20d9
Tomáš Sobota <67016313+sobotat@users.noreply.github.com>
2023-11-02 15:02:28 +0100

    Some Tests
    Took 23 seconds

--- lib/ui/test/TestScreen.dart
+++ lib/ui/test/TestScreen.dart
@@ -1,7 +1,13 @@
 
 import 'package:flutter/material.dart';
-import 'package:oes/src/restApi/api/TestApi.dart';
+import 'package:oes/config/AppApi.dart';
+import 'package:oes/src/objects/Achievement.dart';
+import 'package:oes/ui/assets/dialogs/AchievementDialog.dart';
+import 'package:oes/ui/assets/dialogs/Toast.dart';
 import 'package:oes/ui/assets/templates/AppAppBar.dart';
+import 'package:signalr_netcore/hub_connection.dart';
+import 'package:signalr_netcore/hub_connection_builder.dart';
+import 'package:signalr_netcore/itransport.dart';
 
 class TestScreen extends StatefulWidget {
   const TestScreen({super.key});
@@ -12,57 +18,93 @@ class TestScreen extends StatefulWidget {
 
 class _TestScreenState extends State<TestScreen> {
 
-  List<String> users = [];
-  bool loadingUser = false;
-  int page = 1;
+  List<String> messages = [];
+
+  HubConnection? connection;
 
   @override
   void initState() {
     super.initState();
-    loadUser();
+    initSignalIr();
+
+
+    Future.delayed(Duration(seconds: 2), () {
+      Achievement a = Achievement.fromJson({'id':'123', 'name': 'name', 'description': '12312', 'unlocked': false});
+      AchievementDialog.show(context: context, achievement: a);
+    },);
   }
 
-  Future<void> loadUser() async {
-    setState(() { loadingUser = true; });
-    users = await TestApi.users(page, 2);
+  Future<void> initSignalIr() async {
+    connection = HubConnectionBuilder()
+        .withUrl('${AppApi.instance.apiServerUrl}/testing', transportType: HttpTransportType.LongPolling)
+        .withAutomaticReconnect()
+        .build();
+    connection!.onreconnecting(({error}) {
+      print('Reconnecting');
+    });
 
-    if (users.isNotEmpty) {
-      page += 1;
-    } else {
-      page = 1;
-    }
+    connection!.onreconnected(({connectionId}) {
+      print('Reconnected');
+    });
 
-    setState(() { loadingUser = false; });
+    connection!.onclose(({error}) {
+      if (mounted) {
+        setState(() {});
+        Toast.makeToast(text: 'Connection Closed', context: context);
+      }
+      print('Connection Closed');
+    },);
+
+    await connection!.start();
+    connection!.on('test', (arguments) {
+      if (mounted) {
+        setState(() {});
+        Toast.makeToast(text: 'Message: $arguments', context: context);
+      }
+      print('Message: $arguments');
+    });
+    setState(() {});
+  }
+
+  Future<void> clicked() async {
+    await connection!.invoke('SendMessage').onError((error, stackTrace) {
+      if (mounted) {
+        setState(() {});
+        Toast.makeToast(text: 'Error Invoke: $error', context: context);
+      }
+      print('Error Invoke: $error');
+    });
+    setState(() {});
   }
   
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppAppBar(),
+      appBar: const AppAppBar(),
       body: Center(
         child: Column(
           children: [
             Padding(
               padding: const EdgeInsets.all(10),
               child: InkWell(
+                onTap: () => clicked(),
                 borderRadius: BorderRadius.circular(10),
-                onTap: () => loadUser(),
                 child: Ink(
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(10),
-                    color: Colors.teal[300],
+                    color: connection == null ? Colors.red[300] : connection!.state == HubConnectionState.Connected ? Colors.greenAccent : Colors.orange
                   ),
                   width: 300,
-                  height: 100,
+                  height: 500,
                   child: Align(
                     alignment: Alignment.center,
-                    child: !loadingUser ? ListView.builder(
-                      itemCount: users.length,
+                    child: ListView.builder(
+                      itemCount: messages.length,
                       shrinkWrap: true,
                       itemBuilder: (context, index) {
-                        return Center(child: Text(users[index]));
+                        return Center(child: Text(messages[index]));
                       },
-                    ) : const CircularProgressIndicator(),
+                    ),
                   ),
                 ),
               ),
