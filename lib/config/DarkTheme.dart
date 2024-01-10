a6db99197e2b56f145eff4fdeaa0384191bb7588
Tomáš Sobota <67016313+sobotat@users.noreply.github.com>
2024-01-10 13:27:09 +0100

    Brighten DarkTheme
    Took 11 minutes

--- lib/config/DarkTheme.dart
+++ lib/config/DarkTheme.dart
@@ -7,10 +7,10 @@ class DarkTheme extends AppTheme {
 
   static final AppTheme instance = DarkTheme();
 
-  final Color _primary = const Color.fromRGBO(23, 24, 27, 1.0);
+  final Color _primary = const Color.fromRGBO(26, 27, 30, 1.0);
   final Color _secondary = const Color.fromRGBO(17, 18, 21, 1.0);
   final Color _accent = const Color.fromRGBO(185, 208, 225, 1);
-  final Color _background = const Color.fromRGBO(24, 25, 28, 1.0);
+  final Color _background = const Color.fromRGBO(28, 29, 32, 1.0);
   final Color _textColorLight = const Color.fromRGBO(250, 250, 250, 1);
   final Color _textColorDark = const Color.fromRGBO(5, 5, 5, 1);
 
