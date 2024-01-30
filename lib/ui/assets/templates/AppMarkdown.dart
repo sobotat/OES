a2d139631ec04050a2f25ff06e367a08df89f958
Tomáš Sobota <67016313+sobotat@users.noreply.github.com>
2024-01-30 19:37:52 +0100

    Changed flipBlocksColors to true in AppMarkdown.dart
    Took 4 hours 22 minutes

--- lib/ui/assets/templates/AppMarkdown.dart
+++ lib/ui/assets/templates/AppMarkdown.dart
@@ -10,7 +10,7 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 class AppMarkdown extends StatelessWidget {
   const AppMarkdown({
     required this.data,
-    this.flipBlocksColors = false,
+    this.flipBlocksColors = true,
     super.key
   });
 
