4c19bb47208ec5f1e0ae5ff95e17a711e8760d61
Tomáš Sobota <67016313+sobotat@users.noreply.github.com>
2024-04-14 00:50:48 +0200

    Modified Quiz.dart

--- lib/src/objects/courseItems/Quiz.dart
+++ lib/src/objects/courseItems/Quiz.dart
@@ -1,5 +1,7 @@
 
 import 'package:oes/src/objects/courseItems/SchedulableItem.dart';
+import 'package:oes/src/objects/questions/Question.dart';
+import 'package:oes/src/objects/questions/QuestionFactory.dart';
 
 class Quiz extends SchedulableItem {
 
@@ -11,17 +13,35 @@ class Quiz extends SchedulableItem {
     required super.isVisible,
     required super.scheduled,
     required super.end,
+    required this.questions,
   }) : super(type: 'quiz');
 
+  List<Question> questions;
+
+  @override
+  Map<String, dynamic> toMap() {
+    return super.toMap()..addAll({
+      'questions': questions.map((e) => e.toMap()).toList(),
+    });
+  }
+
   factory Quiz.fromJson(Map<String, dynamic> json) {
+    List<Question> questionsList = [];
+    for(Map<String, dynamic> questionJson in json['questions'] ?? []) {
+      Question? question = QuestionFactory.fromJson(questionJson);
+      if (question == null) continue;
+      questionsList.add(question);
+    }
+
     return Quiz(
-        id: json['id'],
-        name: json['name'],
-        created: DateTime.tryParse(json['created'])!,
-        createdById: json['createdById'],
-        isVisible: json['isVisible'],
-        scheduled: DateTime.tryParse(json['scheduled'])!,
-        end: DateTime.tryParse(json['end'])!,
+      id: json['id'],
+      name: json['name'],
+      created: DateTime.tryParse(json['created'])!,
+      createdById: json['createdById'],
+      isVisible: json['isVisible'],
+      scheduled: DateTime.tryParse(json['scheduled'])!,
+      end: DateTime.tryParse(json['end'])!,
+      questions: questionsList,
     );
   }
 }
