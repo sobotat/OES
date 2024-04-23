4cf4171d5f70c0804a1d44f8c7b74ce26dc5a6fd
Tomáš Sobota <67016313+sobotat@users.noreply.github.com>
2024-04-23 10:43:57 +0200

    Changed connection to private in SignalR.dart

--- lib/src/services/SignalR.dart
+++ lib/src/services/SignalR.dart
@@ -13,18 +13,18 @@ class SignalR {
     this.onReconnected,
     this.onClosed,
   }) {
-    connection = connect(url);
+    _connection = _connect(url);
   }
 
   String url;
-  late HubConnection connection;
+  late HubConnection _connection;
   Map<String, Function(List<dynamic> arguments)> listeners = {};
 
   Function()? onReconnecting;
   Function()? onReconnected;
   Function()? onClosed;
 
-  HubConnection connect(String url) {
+  HubConnection _connect(String url) {
     HubConnection connection = HubConnectionBuilder()
         .withUrl('${AppApi.instance.apiServerUrl}/$url',
           options: HttpConnectionOptions(
@@ -53,9 +53,9 @@ class SignalR {
 
   Future<bool> start(Map<String, Function(List<dynamic> arguments)> listeners) async {
     this.listeners = listeners;
-    await connection.start();
+    await _connection.start();
     for (MapEntry<String, Function> entry in listeners.entries) {
-      connection.on(entry.key, (arguments) {
+      _connection.on(entry.key, (arguments) {
         entry.value(arguments ?? []);
       });
     }
@@ -63,16 +63,16 @@ class SignalR {
   }
 
   Future<void> reset() async {
-    connection = connect(url);
+    _connection = _connect(url);
     await start(listeners);
   }
 
   Future<void> stop() async {
-    await connection.stop();
+    await _connection.stop();
   }
 
   Future<void> send(String message, {List<Object>? arguments}) async {
-    await connection.invoke(message, args: arguments)
+    await _connection.invoke(message, args: arguments)
       .onError((error, stackTrace) {
         print("⭕ Send $message Failed: $error");
         return null;
@@ -80,7 +80,7 @@ class SignalR {
   }
 
   HubConnectionState getState() {
-    return connection.state ?? HubConnectionState.Disconnected;
+    return _connection.state ?? HubConnectionState.Disconnected;
   }
 
 }
