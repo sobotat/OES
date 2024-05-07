433b9ed75c63927f5f938ae2b6cd6772665f97bf
Tomáš Sobota <67016313+sobotat@users.noreply.github.com>
2024-05-07 12:18:59 +0200

    Removed useless imports

--- lib/src/services/NewTabOpener.dart
+++ lib/src/services/NewTabOpener.dart
@@ -1,7 +1,4 @@
-import 'package:oes/ui/assets/dialogs/Toast.dart';
 import 'package:universal_html/html.dart' as html;
-import 'package:url_launcher/url_launcher.dart';
-import 'package:flutter/foundation.dart' show kIsWeb;
 
 class NewTabOpener {
 
